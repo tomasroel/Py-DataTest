ee6cba5ddfa45b2c8f5211a0528f52236a6fc26e
Tomás Roel <tomasroel25@gmail.com>
2024-08-31 12:10:52 -0300

    Primer archivo creado

---
+++ Test.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+# Operadores
+
+print(5 == 2)
+print(10 // 3) #Esto es una division floor
+
+print("Hola" + " Python") #Concateno strings con esto. No puedo restar, solo el + es igual a contactenar
+print("Hola"*2) #Se puede multiplicar un string
+
+# Operadores de comparacion:
+
+print(3 <= 4)
